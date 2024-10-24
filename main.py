b6ee8ee232311da8753f00434222a39c68c872ba
Sammy <bavesh2006kumar@gmail.com>
2024-10-24 11:34:43 +0000

    .

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pyttsx3 # for more info check this website (https://pypi.org/project/pyttsx3/ )
 engine = pyttsx3.init()
 engine.say("I will speak this text")
+engine.say("I will speak this text")
+engine.say("I will speak this text")
 engine.runAndWait()
